b8f0982340ad6d2c20791603b56e9d839dbc2ebe
carbonsoda <carbonsoda@outlook.com>
2025-06-07 14:32:53 +0800

    setup linting and precommit hook

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import eslint from "@eslint/js";
+import tslint from "typescript-eslint";
+
+import stylistic from "@stylistic/eslint-plugin";
+
+import prettier from "eslint-config-prettier";
+
+export default tslint.config(
+	{ files: ["src/**/*.ts"] },
+	{ ignores: ["dist/**", "*.config.mjs"] },
+
+	eslint.configs.recommended,
+	tslint.configs.eslintRecommended,
+
+	tslint.configs.strictTypeChecked,
+	tslint.configs.stylisticTypeChecked,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+
+	stylistic.configs.recommended,
+
+	prettier,
+);
