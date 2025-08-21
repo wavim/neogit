f3923143d10b29aa27383317e0b0088a0f670b7e
carbonsoda <carbonsoda@outlook.com>
2025-08-21 18:54:43 +0800

    refactor: rework pack class

--- src/core/read-object/pack.ts
+++ src/core/read-object/pack.ts
@@ -7,74 +7,69 @@ export interface Packed {
 
 export class Pack {
 	readonly ofsMap = new Map<string, number>();
-	readonly ofsEnd = new Map<number, number>();
+	readonly objLen = new Map<number, number>();
 
 	constructor(
-		idxBuffer: Buffer,
+		index: Buffer,
 		readonly pack: string,
 	) {
-		const oidFanout = idxBuffer.subarray(2 * 4, 2 * 4 + 256 * 4);
-		const oidCount = oidFanout.readUint32BE(255 * 4);
+		const firstFan = index.subarray(8, 1032);
+		const oidCount = firstFan.readUint32BE(1020);
 
-		const oidLookup = idxBuffer.subarray(
-			2 * 4 + 256 * 4,
-			2 * 4 + 256 * 4 + oidCount * 20,
-		);
-		const ofsLookup = idxBuffer.subarray(
-			2 * 4 + 256 * 4 + oidCount * 20 + oidCount * 4,
-			2 * 4 + 256 * 4 + oidCount * 20 + oidCount * 4 + oidCount * 4,
-		);
+		const oidLookup = index.subarray(1032, 1032 + oidCount * 20);
+		const ofsLookup = index.subarray(1032 + oidCount * 24, 1032 + oidCount * 28);
 
-		const ofsList = new Uint32Array(oidCount);
+		const ofsLinked = new Uint32Array(oidCount);
 
 		for (let i = 0; i < oidCount; i++) {
 			const oid = oidLookup.toString("hex", i * 20, i * 20 + 20);
 			const ofs = ofsLookup.readUint32BE(i * 4);
 
 			if (ofs & 0x80_00_00_00) {
+				// skips >2GiB pack indices
 				continue;
 			}
 
+			ofsLinked[i] = ofs;
 			this.ofsMap.set(oid, ofs);
-			ofsList[i] = ofs;
 		}
-
-		ofsList.sort();
+		ofsLinked.sort();
 
 		for (let i = 0; i < oidCount; i++) {
-			this.ofsEnd.set(ofsList[i], ofsList[i + 1] ?? Infinity);
+			const ofs = ofsLinked[i];
+			const end = ofsLinked[i + 1] ?? Infinity;
+
+			this.objLen.set(ofs, end - ofs);
 		}
 	}
 
-	async queryRef(ref: string): Promise<Packed | undefined> {
-		const offset = this.ofsMap.get(ref);
+	async ofs(offset: number): Promise<Packed | undefined> {
+		let objLen = this.objLen.get(offset);
+
+		if (objLen === undefined) {
+			return undefined;
+		}
+		const pack = await open(this.pack);
+
+		if (objLen === Infinity) {
+			const stats = await pack.stat();
+			objLen = stats.size;
+		}
+
+		const buffer = Buffer.allocUnsafe(objLen);
+
+		await pack.read({ buffer, position: offset });
+		await pack.close();
+
+		return { offset, buffer };
+	}
+
+	async ref(objref: string): Promise<Packed | undefined> {
+		const offset = this.ofsMap.get(objref);
 
 		if (offset === undefined) {
 			return undefined;
 		}
-
-		return await this.queryOfs(offset);
-	}
-
-	async queryOfs(ofs: number): Promise<Packed | undefined> {
-		let end = this.ofsEnd.get(ofs);
-
-		if (end === undefined) {
-			return undefined;
-		}
-
-		const handle = await open(this.pack);
-
-		if (end === Infinity) {
-			const stats = await handle.stat();
-			end = stats.size;
-		}
-
-		const buffer = Buffer.alloc(end - ofs);
-
-		await handle.read(buffer, { position: ofs });
-		await handle.close();
-
-		return { offset: ofs, buffer };
+		return await this.ofs(offset);
 	}
 }
