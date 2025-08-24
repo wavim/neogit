47b1c8f0c971b6b63227ae605518bcca4068a2fa
carbonsoda <carbonsoda@outlook.com>
2025-08-24 12:15:24 +0800

    feat: graph parent map

--- src/lib/read-graph/graph.ts
+++ src/lib/read-graph/graph.ts
@@ -3,6 +3,7 @@ import { join } from "node:path";
 
 export class Graph {
 	readonly genMap = new Map<string, number>();
+	readonly ptsMap = new Map<string, string | null>();
 
 	static async build(repo: string): Promise<Graph | null> {
 		const path = join(repo, ".git", "objects", "info", "commit-graph");
@@ -24,15 +25,34 @@ export class Graph {
 			i + 1024 + oidNumber * 56,
 		);
 
+		const array = [];
+
 		for (let i = 0; i < oidNumber; i++) {
 			const oid = oidLookup.toString("hex", i * 20, i * 20 + 20);
-			const gen = cdatChunk.readUint32BE(i * 36 + 28) >> 2;
+			array.push(oid);
+		}
+
+		for (let i = 0; i < oidNumber; i++) {
+			const j = i * 36;
+
+			const oid = array[i];
+			const pt1 = cdatChunk.readUint32BE(j + 20);
+			const gen = cdatChunk.readUint32BE(j + 28) >> 2;
 
 			this.genMap.set(oid, gen);
+
+			if (cdatChunk.readUint8(j + 24) & 0x80) {
+				continue;
+			}
+			this.ptsMap.set(oid, pt1 === 0x70_00_00_00 ? null : array[pt1]);
 		}
 	}
 
 	findGen(objref: string): number | undefined {
 		return this.genMap.get(objref);
+	}
+
+	findPts(objref: string): string | undefined | null {
+		return this.ptsMap.get(objref);
 	}
 }
